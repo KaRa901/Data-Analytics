bb51c3ad4329864027372fa6d54079e5a8467fdb
KaRa901 <117648802+KaRa901@users.noreply.github.com>
2024-05-01 11:20:50 +0200

    Update main_datainput.py

--- BachelorThesis/main_datainput.py
+++ BachelorThesis/main_datainput.py
@@ -1,1 +1,8 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import astropy as ap
+from astropy.io import fits
+from astropy.convolution import convolve, Gaussian2DKernel
 
+hdul=ap.io.fits.open('lsdcat0138_minicube.fits')
+data=hdul[1].data
