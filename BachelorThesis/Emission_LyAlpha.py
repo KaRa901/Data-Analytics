350f20d5dc9fc5ab3a63f99e65d314c9f453c448
KaRa901 <117648802+KaRa901@users.noreply.github.com>
2024-05-02 14:17:33 +0200

    Update Emission_LyAlpha.py

--- BachelorThesis/Emission_LyAlpha.py
+++ BachelorThesis/Emission_LyAlpha.py
@@ -1,1 +1,22 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import astropy as ap
+from astropy.io import fits
+from astropy.convolution import convolve, Gaussian2DKernel
 
+hdul=ap.io.fits.open('lsdcat0138_minicube.fits')
+data=hdul[1].data
+hdul1=ap.io.fits.open('lsdcat0138_mfscube.fits')
+mfscube=hdul1[1].data
+
+mfcube=data-mfscube
+
+xc,yc=60,60
+l1dla,l2dla=250,290
+yy,xx=np.indices(mfcube[0].shape)
+rr=np.sqrt((xx-xc)**2+(yy-yc)**2)
+mfmaxrad=5.5
+rsel=rr<mfmaxrad
+mfcube[l1dla:l2dla,rsel]=0
+
+mfsmodcube=data-mfcube
